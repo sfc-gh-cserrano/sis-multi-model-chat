753ce5a274301c9136f056c257bd57ef73e33530
sfc-gh-cserrano <carlos.serrano@snowflake.com>
2024-07-30 09:18:21 -0400

    Adding to repo

---
+++ Readme.md
@@ -0,0 +1,65 @@
+# Multi-Model Chat using Snowpark Async
+
+Submits prompts to multiple LLMs using Streamlit Chat Widgets, Snowflake's Complete function through Snowpark Async Methods.
+
+
+
+## Deployment Option 1:
+
+Create Streamlit app using Snowsight and replace default app code with code from `streamlit_app.py` file. 
+
+## Deployment Option 2:
+
+1- Create a Snowflake Stage with Directory enabled. 
+```sql
+CREATE OR REPLACE STAGE DB.ST_APPS.STAGE_NAME
+DIRECTORY=(ENABLE=TRUE);
+```
+2- Upload the `streamli_app.py` and `environment.yml` files into the root of the stage. 
+3- Create Streamlit app using the code below. 
+```sql
+CREATE STREAMLIT ASYNC_MULTI_MODEL_CHAT
+  ROOT_LOCATION = '@DB.ST_APPS.STAGE_NAME'
+  MAIN_FILE = 'streamlit_app.py'
+  QUERY_WAREHOUSE = STREAMLIT_XS;
+```
+
+## Basic Process:
+
+```mermaid
+graph TB;
+
+step1[Select Models]
+step2[Submit Prompt]
+step3[Compose Prompt]
+step4[For N models in Models] 
+step5[Submit Final Prompt]
+step6[Add to Prompt History]
+step7{Submit all?}
+step9[Render N number of columns]
+step10[For N models in Models] 
+step11{is async_job}
+step12{is_done}
+step13[mark as pending]
+step14[replace async job with final response]
+step15[display results]
+step16[display waiting placeholder]
+step17{any pending?}
+step18[wait 5 seconds]
+step19[[done]]
+
+step1-->step2-->step3-->step4-->step5-->step6-->step7
+
+step7--N-->step4
+step7--Y-->step9
+step9-->step10
+step10-->step11
+step11--N-->step15
+step11--Y-->step12
+step12--Y-->step14-->step15
+step12--N-->step13-->step16
+step16-->step17
+step15-->step17
+step17--N-->step19
+step17--Y-->step18-->step10
+```
---
+++ streamlit_app.py
@@ -0,0 +1,134 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from enum import Enum
+from snowflake.snowpark.exceptions import SnowparkSQLException
+from snowflake.snowpark.async_job import AsyncJob
+import time
+
+st.set_page_config(layout="wide")
+session = get_active_session()
+
+if "prompt_history" not in st.session_state:
+    st.session_state["prompt_history"] = {}
+
+
+class SystemMessages(Enum):
+    DEFAULT = """Act as a Subject matter expert. 
+                 Provide simple explanations.
+                 Do Not return System Roles"""
+
+
+def reset():
+    st.session_state["prompt_history"] = {}
+
+
+def submit_prompt(prompt: str) -> None:
+    """Submits prompt to LLM using `to_pandas() method with `block=false` argument. Returns a Snowpark Async Job"""
+    for idx, model in enumerate(st.session_state["selected_models"]):
+        try:
+            with st.spinner("Awaiting response"):
+                comp_prompt = compose_prompt(st.session_state[prompt])
+                response = session.sql(
+                    "select snowflake.cortex.complete(?, ?) as RESPONSE",
+                    params=[model, comp_prompt],
+                ).to_pandas(block=False)
+                st.session_state["prompt_history"][idx].append(
+                    dict(prompt=st.session_state[prompt], response=response)
+                )
+        except Exception as e:
+            st.exception(e)
+
+
+def compose_prompt(prompt: str) -> str:
+    """Composes a prompt text adding system roles."""
+    full_prompt = [{"role": "system", "content": SystemMessages.DEFAULT.value}]
+    user_prompt = [{"role": "user", "content": prompt}]
+    full_prompt.extend(user_prompt)
+    return str(full_prompt)
+
+
+models = sorted(
+    [
+        "llama3-8b",
+        "llama3-70b",
+        "snowflake-arctic",
+        "reka-core",
+        "reka-flash",
+        "mistral-large",
+        "mixtral-8x7b",
+        "llama2-70b-chat",
+        "mistral-7b",
+        "gemma-7b",
+        "llama3.1-405b",
+    ]
+)
+
+
+pending_reponses = 0
+selected_models = st.multiselect(
+    "LLM Model",
+    options=models,
+    key="selected_models",
+    max_selections=4,
+    on_change=reset,
+)
+menu = st.columns((1, 5))
+with menu[0].container(border=True):
+    show_all = st.toggle("Show all", value=False)
+if selected_models:
+    with st.container(border=True):
+        reponses_columns = st.columns(len(selected_models))
+        for column, reponses in st.session_state["prompt_history"].items():
+            with reponses_columns[column]:
+                st.subheader(selected_models[column], anchor=False, divider=True)
+                model_responses = (
+                    st.session_state["prompt_history"][column]
+                    if show_all
+                    else st.session_state["prompt_history"][column][-1:]
+                )
+                for row, chat in enumerate(model_responses):
+                    if chat.get("prompt"):
+                        with st.chat_message("user"):
+                            st.write(chat.get("prompt"))
+                    if chat.get("response"):
+                        if isinstance(chat.get("response"), AsyncJob):
+                            if chat.get("response").is_done():
+                                try:
+                                    model_responses[row]["response"] = chat.get(
+                                        "response"
+                                    ).result()["RESPONSE"][0]
+                                except Exception as e:
+                                    if "unknown model" in e.__repr__():
+                                        model_responses[row][
+                                            "response"
+                                        ] = "`Model Not Available`"
+                                    else:
+                                        "`Unknown Error`"
+                                with st.chat_message("ai"):
+                                    st.write(chat.get("response"))
+                            else:
+                                with st.chat_message("ai"):
+                                    pending_reponses += 1
+                                    st.write("Awaiting response...")
+                        else:
+                            with st.chat_message("ai"):
+                                st.write(chat.get("response"))
+if pending_reponses > 0:
+    time_left = 5
+    prog_cont = menu[1].empty()
+    for percent_complete in range(5):
+
+        prog_cont.info(
+            f"**Pending prompts, will check for progress in :orange[{time_left-percent_complete}] seconds**"
+        )
+        time.sleep(1)
+        prog_cont.empty()
+    st.rerun()
+
+st.chat_input(
+    "Ask me anything",
+    on_submit=submit_prompt,
+    key="prompt_input",
+    args=["prompt_input"],
+)
